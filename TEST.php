d9aef342646df6f4a3cf72f6bef9a48f630dcc83
derrondad <derrondad@yahoo.com>
2022-09-21 14:41:59 +0100

    Add files via upload

---
+++ TEST.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    </head>
+    <body>
+        <?php
+            define('COLLEGE','TMC City centre campus');
+            echo "<p>This is a list of students at" .COLLEGE . '</p>';
+        ?>
+    </body>
+</html>
